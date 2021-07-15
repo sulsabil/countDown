27338d767797468e9bce06b2de5d755b08d4e0a2
SABILA <salsabilnishat@gmail.com>
2021-07-16 03:20:16 +0600

    first commit

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ navToggle.addEventListener("click", function () {
 var countDate = new Date("july 16,2021 00:00:00").getTime();
 console.log(countDate);
 
-const time = setInterval(function(){
-    const now =new Date().getTime();
-    const diff = countDate - now;
-    console.log(diff);
+const time = setInterval(function () {
+  const now = new Date().getTime();
+  const diff = countDate - now;
+  console.log(diff);
 
-    const textDay = Math.round(diff / (1000 * 60 * 60 * 24));
-    const textHour = Math.round((diff % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-    const textMinute = Math.round((diff % (1000* 60 * 60))/(1000 * 60));
-    const textSecond = Math.round((diff %(1000 * 60))/1000); 
-    document.querySelector(".day").innerText = textDay;
-    document.querySelector(".hour").innerText = textHour;
-    document.querySelector(".minute").innerText = textMinute;
-    document.querySelector(".second").innerText = textSecond;
+  const textDay = Math.round(diff / (1000 * 60 * 60 * 24));
+  const textHour = Math.round((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const textMinute = Math.round((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const textSecond = Math.round((diff % (1000 * 60)) / 1000);
+  
+  document.querySelector(".day").innerText = textDay;
+  document.querySelector(".hour").innerText = textHour;
+  document.querySelector(".minute").innerText = textMinute;
+  document.querySelector(".second").innerText = textSecond;
 
-                
-},1000)
+
+}, 1000)
